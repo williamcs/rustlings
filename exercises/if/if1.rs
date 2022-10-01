d129760f732d2007d3bc8aa998955388ea695e1e
williamcs <solobird00@hotmail.com>
2022-10-01 17:27:53 +0800

    first commit for solving some problems

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -8,6 +8,10 @@ pub fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b {
+        return a;
+    }
+    b
 }
 
 // Don't mind this for now :)
